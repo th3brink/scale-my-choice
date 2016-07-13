7dad060800ed369fc34b793a9d88a76109a34f9a
James Brinkerhoff <th3brink@gmail.com>
2016-07-13 16:49:27 -0600

    finished create user auth

--- app/pages/auth/auth.ts
+++ app/pages/auth/auth.ts
@@ -5,12 +5,17 @@ import {AngularFire} from 'angularfire2';
 import {Observable} from 'rxjs/Observable';
 import {TabsPage} from '../tabs/tabs';
 
+interface EmailPasswordCredentials {
+    email: string;
+    password: string;
+}
+
 @Component({
   template: `
     <ion-header>
         <ion-navbar>
             <ion-title>
-            Login
+            {{state}}
             </ion-title>
         </ion-navbar>
     </ion-header>
@@ -28,16 +33,29 @@ import {TabsPage} from '../tabs/tabs';
             <ion-input type="password" [(ngModel)]="user.password" ></ion-input>
         </ion-item>
 
+        <ion-item *ngIf="state === 'Create'">
+            <ion-label>Password Again</ion-label>
+            <ion-input type="password" [(ngModel)]="passwordCheck" ></ion-input>
+        </ion-item>
+
         </ion-list>
 
-        <div padding>
-            <button block (click)="login()">Sign In</button>
+        <div padding *ngIf="state === 'Login'">
+            <button block (click)="Login()">Sign In</button>
+            <button block light (click)="switchState('Create')">Create Account</button>
+        </div>  
+
+        <div padding *ngIf="state === 'Create'">
+            <button block (click)="create()">Create Account</button>
+            <button block light (click)="switchState('Login')">Sign In</button>
         </div>  
     </ion-content>
   `
 })
 export class AuthPage {
-  user: Object = {email: '', password: ''};
+  user: EmailPasswordCredentials = {email: '', password: ''};
+  passwordCheck: string = '';
+  state: string = 'Login'; //Create
   constructor(private nav: NavController, public af: AngularFire) {
     
   }
@@ -61,6 +79,43 @@ export class AuthPage {
 
     });
     
+  }
+
+  create() {
+    if (this.user.password === '' || this.user.password !== this.passwordCheck) {
+        let toast = Toast.create({
+            message: "Passowrds don't match",
+            duration: 3000
+        });
+        toast.onDismiss(() => {
+            console.log('Dismissed toast');
+        });
+        this.nav.present(toast);
+        return;
+    }
+    this.af.auth.createUser(this.user)
+    .then(()=>{
+        this.nav.setPages([ {page: TabsPage} ]);
+    })
+    .catch((err)=>{
+        console.log(err);
+        let toast = Toast.create({
+            message: 'create failed',
+            duration: 3000
+        });
+
+        toast.onDismiss(() => {
+            console.log('Dismissed toast');
+        });
+
+        this.nav.present(toast);
+
+    });
+    
+  }
+
+  switchState(state) {
+    this.state = state;
   }
 
 }
